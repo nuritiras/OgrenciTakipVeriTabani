db9fed417b9c562afb61513b0040debaaa2d94fc
nuri <nuri@ROGRT>
2024-05-07 11:49:01 +0300

    Proje dosyası ekle.

---
+++ OgrenciTakip/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace OgrenciTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAdiSoyadi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxOkulNo = new System.Windows.Forms.TextBox();
+            this.buttonKaydet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ogrenciDbDataSet = new OgrenciTakip.OgrenciDbDataSet();
+            this.ogrencilerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ogrencilerTableAdapter = new OgrenciTakip.OgrenciDbDataSetTableAdapters.OgrencilerTableAdapter();
+            this.buttonListele = new System.Windows.Forms.Button();
+            this.ıdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.okulNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adiSoyadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrenciDbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrencilerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı Soyadı:";
+            // 
+            // textBoxAdiSoyadi
+            // 
+            this.textBoxAdiSoyadi.Location = new System.Drawing.Point(189, 84);
+            this.textBoxAdiSoyadi.Name = "textBoxAdiSoyadi";
+            this.textBoxAdiSoyadi.Size = new System.Drawing.Size(251, 20);
+            this.textBoxAdiSoyadi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Okul No:";
+            // 
+            // textBoxOkulNo
+            // 
+            this.textBoxOkulNo.Location = new System.Drawing.Point(189, 58);
+            this.textBoxOkulNo.Name = "textBoxOkulNo";
+            this.textBoxOkulNo.Size = new System.Drawing.Size(134, 20);
+            this.textBoxOkulNo.TabIndex = 1;
+            // 
+            // buttonKaydet
+            // 
+            this.buttonKaydet.Location = new System.Drawing.Point(189, 127);
+            this.buttonKaydet.Name = "buttonKaydet";
+            this.buttonKaydet.Size = new System.Drawing.Size(75, 23);
+            this.buttonKaydet.TabIndex = 2;
+            this.buttonKaydet.Text = "Kaydet";
+            this.buttonKaydet.UseVisualStyleBackColor = true;
+            this.buttonKaydet.Click += new System.EventHandler(this.buttonKaydet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıdDataGridViewTextBoxColumn,
+            this.okulNoDataGridViewTextBoxColumn,
+            this.adiSoyadiDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ogrencilerBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(327, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(433, 247);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Visible = false;
+            // 
+            // ogrenciDbDataSet
+            // 
+            this.ogrenciDbDataSet.DataSetName = "OgrenciDbDataSet";
+            this.ogrenciDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ogrencilerBindingSource
+            // 
+            this.ogrencilerBindingSource.DataMember = "Ogrenciler";
+            this.ogrencilerBindingSource.DataSource = this.ogrenciDbDataSet;
+            // 
+            // ogrencilerTableAdapter
+            // 
+            this.ogrencilerTableAdapter.ClearBeforeFill = true;
+            // 
+            // buttonListele
+            // 
+            this.buttonListele.Location = new System.Drawing.Point(189, 165);
+            this.buttonListele.Name = "buttonListele";
+            this.buttonListele.Size = new System.Drawing.Size(75, 23);
+            this.buttonListele.TabIndex = 4;
+            this.buttonListele.Text = "Listele";
+            this.buttonListele.UseVisualStyleBackColor = true;
+            this.buttonListele.Click += new System.EventHandler(this.buttonListele_Click);
+            // 
+            // ıdDataGridViewTextBoxColumn
+            // 
+            this.ıdDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.ıdDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.ıdDataGridViewTextBoxColumn.Name = "ıdDataGridViewTextBoxColumn";
+            this.ıdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // okulNoDataGridViewTextBoxColumn
+            // 
+            this.okulNoDataGridViewTextBoxColumn.DataPropertyName = "OkulNo";
+            this.okulNoDataGridViewTextBoxColumn.HeaderText = "Okul No";
+            this.okulNoDataGridViewTextBoxColumn.Name = "okulNoDataGridViewTextBoxColumn";
+            // 
+            // adiSoyadiDataGridViewTextBoxColumn
+            // 
+            this.adiSoyadiDataGridViewTextBoxColumn.DataPropertyName = "AdiSoyadi";
+            this.adiSoyadiDataGridViewTextBoxColumn.HeaderText = "Adı Soyadı";
+            this.adiSoyadiDataGridViewTextBoxColumn.Name = "adiSoyadiDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonListele);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonKaydet);
+            this.Controls.Add(this.textBoxOkulNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxAdiSoyadi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrenciDbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrencilerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAdiSoyadi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOkulNo;
+        private System.Windows.Forms.Button buttonKaydet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private OgrenciDbDataSet ogrenciDbDataSet;
+        private System.Windows.Forms.BindingSource ogrencilerBindingSource;
+        private OgrenciDbDataSetTableAdapters.OgrencilerTableAdapter ogrencilerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn okulNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiSoyadiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonListele;
+    }
+}
+
---
+++ OgrenciTakip/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OgrenciTakip
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection(@"Data Source=tsomtal.com;Initial Catalog=OgrenciDb;Integrated Security=True;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKaydet_Click(object sender, EventArgs e)
+        {
+            try { 
+                baglanti.Open();
+                int OkulNo=Convert.ToInt32(textBoxOkulNo.Text);
+                string sqlCumlesi="INSERT INTO Ogrenciler (OkulNo,AdiSoyadi) VALUES("+OkulNo+",'" + textBoxAdiSoyadi.Text+"')";
+                SqlCommand cmd = new SqlCommand(sqlCumlesi,baglanti);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Veriler aktarıldı.");
+            } catch (Exception ex) {
+                MessageBox.Show("Bağlantı hatası\n" + ex.Message);
+            }
+            finally
+            {
+               if (baglanti!=null) baglanti.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'ogrenciDbDataSet.Ogrenciler' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.ogrencilerTableAdapter.Fill(this.ogrenciDbDataSet.Ogrenciler);
+
+        }
+
+        private void buttonListele_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+            
+        }
+    }
+}
